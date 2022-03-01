b82a09ba0b042654b97f788fa731ce5f5dd324e1
Dattn0801 <Dattn0801>
2022-03-01 11:03:59 +0700

    ngFor

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -8,9 +8,18 @@ import { Component, OnInit } from '@angular/core';
 export class HomeComponent implements OnInit {
   public name = 'Dat';
   public age = 25;
+  public traiCay =['Tao','Cam','Nho','Quyt'];
+  public traiCay2=[
+    {ten:'Nho', gia:12, haGia: true},
+    {ten:'Cam', gia: -1, haGia: false},
+    {ten:'Oi', gia: -1, haGia: true},
+
+  ];
+ 
   constructor() { }
 
   ngOnInit(): void {
+    console.log('Trai Cay =',this.traiCay)
   }
   public resetName():void{
     console.log('resetName');
