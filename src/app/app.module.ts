8bb9a4ec9619373c20981fc70b002eec3cd20c78
Dattn0801 <Dattn0801>
2022-03-05 12:57:18 +0700

    ngSwitch

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,8 @@ import { AboutComponent } from './about/about.component';
 import { FormsModule } from '@angular/forms';
 import { OnSalePipe } from './on-sale.pipe';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { WarningComponent } from './warning/warning.component';
+import { SuccessComponent } from './success/success.component';
 
 @NgModule({
   declarations: [
@@ -15,7 +17,9 @@ import { PageNotFoundComponent } from './page-not-found/page-not-found.component
     HomeComponent,
     AboutComponent,
     OnSalePipe,
-    PageNotFoundComponent
+    PageNotFoundComponent,
+    WarningComponent,
+    SuccessComponent
   ],
   imports: [
     BrowserModule,
